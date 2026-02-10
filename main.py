4f674eb4bb8ec0edf3c9c825c797fcc38338a0f4
Ahsanakbar757 <165800593+Ahsanakbar757@users.noreply.github.com>
2026-02-10 23:52:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -149,6 +149,5 @@ def askQuestion():
 def home():
     return "<h1>Gemini RAG API is Online </h1>"
     
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000)) 
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
